07b5d55d2049e0296241b204f3c27168a931d9e7
tbningrat12 <hamzahmaulana212@gmail.com>
2023-08-28 14:30:08 +0700

    proptype contactitem

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ContactItemBody from './ContactItemBody';
 import ContactItemImage from './ContactItemImage';
 import DeleteButton from './DeleteButton';
-
+import PropTypes from 'prop-types';
 
 function ContactItem({ imageUrl, name, tag, id, onDelete }) {
  return (
@@ -14,6 +14,13 @@ function ContactItem({ imageUrl, name, tag, id, onDelete }) {
  );
 }
 
+ContactItem.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired,
+}
 
 
 export default ContactItem;
